0f125f9dcbed398b80fde6877a8e662ab13c2afd
Saniyakhan04 <saniya.khan0414@gmail.com>
2024-02-14 22:56:21 +0530

    First ever commit

---
+++ Ash.java
@@ -0,0 +1,5 @@
+public class Ash{
+	public static void main(String[] args){
+		System.out.println("Pika pi pikachu!");
+	}
+}
---
+++ AshApp.java
@@ -0,0 +1,6 @@
+public class AshApp{
+	public static void main(String[] args){
+		System.out.println("Pika pi pikachu!");
+		System.out.println("Some changes!");
+	}
+}
