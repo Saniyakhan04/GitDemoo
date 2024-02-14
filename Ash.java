6ebe898389619e11ec4f1a84d4a2556faff75b5b
Saniyakhan04 <saniya.khan0414@gmail.com>
2024-02-15 00:10:30 +0530

    Modified after cloning

--- Ash.java
+++ Ash.java
@@ -2,4 +2,10 @@ public class Ash{
 	public static void main(String[] args){
 		System.out.println("Pika pi pikachu!");
 	}
+	public static void fun()
+	{
+		int a = 10;
+		int b = 20;
+		int difference = a-b;
+		System.out.println(difference);
 }
